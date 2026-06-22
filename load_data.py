62c002bb4d78cf0e445f2c39c3416384bb0e80ac
koszyk99 <krzysztof_koszyk@interia.eu>
2026-06-22 21:30:03 +0200

    feat: checking negative numbers and clear overwrite them

--- load_data.py
+++ load_data.py
@@ -3,27 +3,20 @@ import numpy as np
 
 df = pd.read_csv('Global_Superstore2.csv', encoding='cp1250')
 
-# Checking table size
-# print(df.shape)
-
-# Display a list of columns, the number of non-null values and data types
-# print(df.info())
-
-# Display first 5 rows to investigate values
-# print(df.head(5))
-
 # Convert date columns to datetime objects
 df['Order Date'] = pd.to_datetime(df['Order Date'], format='%d-%m-%Y')
 df['Ship Date'] = pd.to_datetime(df['Ship Date'], format='%d-%m-%Y')
 
 # Create new column "Shipment Time"
-df['Shipment Time'] = df['Ship Date'] - df['Order Date']
+df['Shipment Time'] = (df['Ship Date'] - df['Order Date']).dt.days
+
+print("[TEST] Checking negative shipping times")
+print("Number of orders with negative shipping times:", (df['Shipment Time'] < 0).sum())
 
 """
 Convert selected columns to 'category' dtype to reduce memory usage
 and improve performance (especially for groupby operations).
 """
-
 category_type = [
     'Ship Mode', 'Segment', 'Category', 'Sub-Category', 
     'Market', 'Region', 'Country', 'Order Priority'
@@ -38,6 +31,23 @@ df['Postal Code'] = df['Postal Code'].astype(str).str.replace('.0', '', regex=Fa
 # Replace text placeholder for missing values with actual NaN
 df['Postal Code'] = df['Postal Code'].replace(['nan', 'NaN', 'NAN', 'None', ''], np.nan)
 
-print(df['Postal Code'].head(5))
+print('-' * 48)
+print("[TEST] Negative financial values test - verify Sales and Quantity columns")
+print('Negative Sales number:', (df['Sales'] <= 0).sum())
+print('Negative Quantity number:', (df['Quantity'] <= 0).sum())
+print('-' * 48)
+
+# If the above test returns negative values, this line of code will clear them - we overwrite DataFrame
+df = df[(df['Sales'] > 0) & (df['Quantity'] > 0)]
+
+# print(df.head(5))
+
+# Checking table size
+# print(df.shape)
+
+# Display a list of columns, the number of non-null values and data types
+# print(df.info())
+
 # print(df.dtypes)
+
 # print(df[['Shipment Time', 'Order Date', 'Ship Date']].describe())
